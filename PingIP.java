f0e18fa9bfaaf5a31eeaa949d8961abc8db150a3
rcastr13 <rcastr13@calstatela.edu>
2017-06-02 12:52:59 -0700

    Add multithread 	-Facebook 	-Google 	-Yahoo

--- PingIP.java
+++ PingIP.java
@@ -1,6 +1,8 @@
 /*
  * From https://gist.github.com/madan712/4509039
  */
+import java.time.Instant;
+import java.time.Duration;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,42 +13,64 @@ import java.io.PrintWriter;
 
 public class PingIP {
 	public static void runSystemCommand(String command, String fileName){
-		try{
-			Process p = Runtime.getRuntime().exec(command);
-			BufferedReader inputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			
-			String s = "";
-			
-			// reading output stream of the command
-			while((s = inputStream.readLine()) != null){
-				// System.out.println(s);
-				writeToFile(s, fileName);
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				try{
+					Process p = Runtime.getRuntime().exec(command);
+					BufferedReader inputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+					String s = "";
+
+				
+					//Reading output stream of the command.
+					while((s = inputStream.readLine()) != null){
+						writeToFile(s, fileName);
+					}
+
+				}catch(Exception e){
+					e.printStackTrace();
+				}
 			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		}).start();
 	}
-  
+ 
 	public static void writeToFile(String s, String fileName) {
-		try(FileWriter fw = new FileWriter(fileName, true);
+		try{
+			FileWriter fw = new FileWriter(fileName, true);
 			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter out = new PrintWriter(bw)){
-			    out.println(s);
-			    out.close();
+			PrintWriter out = new PrintWriter(bw);
+
+			out.println(s);
+			out.close();
 		}catch(IOException e){
-		    
+			System.out.println(e);
 		}
 	}
-	
+
 	public static void sortString() {
-	
+		//?
 	}
 
 	public static void main(String[] args) {
 		String ip = "google.com";
+		String ip2 = "yahoo.com";
+		String ip3 = "facebook.com";
 
 		// Mac
-		runSystemCommand("ping " + "-i 10 " + ip, "google.txt");
+		//Start record of ping time.
+		Instant start = Instant.now();
+		System.out.println("Pinging Google...");
+		runSystemCommand("ping " + "-i 10 " + ip, "pings/google.txt");
+		//End record of pinging.
+		Instant end = Instant.now();
+		Duration duration = Duration.between(start, end);
+
+		System.out.println("Pinging Yahoo...");	
+		runSystemCommand("ping " + "-i 10 " + ip2, "pings/yahoo.txt");
+
+		System.out.println("Pinging Facebook...");
+		runSystemCommand("ping " + "-i 10 " + ip3, "pings/facebook.txt");
 
 		// Windows
 		//runSystemCommand("ping " + "-w 10000 " + ip + " -t", "google.txt");
