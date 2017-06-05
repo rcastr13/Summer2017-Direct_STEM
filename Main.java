a41a8dc3382c5e333b5922b5e4f4c1c933600633
rcastr13 <rcastr13@calstatela.edu>
2017-06-05 09:46:34 -0700

    Add time stamps for pings
    Expanded pings to
    	emails: yahoo.com, hotmail.com (Something went wrong with gmail as it was not recorded)
    	search engines: google.com, yahoo.com, duckduckgo.com
    	games (For fun): store.steampowered.com, store.playstation.com, store.nintendo.com

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 /**
  * From https://gist.github.com/madan712/4509039
  */
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,22 +10,54 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {	
-		Pane root = new Pane();
+/**
+ * Continued JavaFX implementations.
+ */
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
+/**
+ * Thread and File IO implementations.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage){	
+		/**
+		 * The box which contains everything.
+		 */
+		Pane root = new Pane();
+		
+		/**
+		 * Creates the layout of the form using
+		 * the root.
+		 */
 		createText(root);
 		createTextField(root);
 		createPingButton(root);
-
-		Scene scene = new Scene(root, 275, 250);
-
+		
+		/**
+		 * Area to place the form.
+		 */
+		Scene scene = new Scene(root, 300, 200);
+		
+		/**
+		 * Sets the scene to be shown on the pane.
+		 */
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	public void createText(Pane root) {
+	/**
+	 * Texts.
+	 */
+	public void createText(Pane root){
 		Text one = new Text("1st Domain:");
 		one.setLayoutX(10);
 		one.setLayoutY(45);
@@ -41,11 +72,17 @@ public class Main extends Application {
 		three.setLayoutX(10);
 		three.setLayoutY(125);
 		three.setFont(Font.font ("Helvetica", 16));
-
+		
+		/**
+		 * Places all text onto pane.
+		 */
 		root.getChildren().addAll(one, two, three);
 	}
 	
-	public void createTextField(Pane root) {
+	/**
+	 * Input fields.
+	 */
+	public void createTextField(Pane root){
 		TextField domainOne = new TextField();
 		domainOne.setMaxWidth(200);
 		domainOne.setLayoutX(115);
@@ -60,27 +97,104 @@ public class Main extends Application {
 		domainThree.setMaxWidth(200);
 		domainThree.setLayoutX(115);
 		domainThree.setLayoutY(110);
-	
+
+		/**
+		 * Places all text fields onto pane.
+		 */
 		root.getChildren().addAll(domainOne, domainTwo, domainThree);
-		
 	}
 	
-	public void createPingButton(Pane root) {
-		
+	/**
+	 * Button.
+	 */
+	public void createPingButton(Pane root){
 		Button btn = new Button("Start Test");	 
 		btn.setLayoutX(200);
 		btn.setLayoutY(150);
+		
+		SetPingBtnOnClick btnClick = new SetPingBtnOnClick();
 
-		setPingBtnOnClick(root, btn);
+//		btnClick(domainOne.getText());
+//		btnClick(domainTwo.getText());
+//		btnClick(domainThree.getText());
 
+		//setPingBtnOnClick(root, btn);
+
+		btn.setOnAction(btnClick);
+
+		/**
+		 * Places button onto pane.
+		 */
 		root.getChildren().add(btn);
 	}
+	
+	///**
+	// * Event handler.
+	// */
+	//public void setPingBtnOnClick(Pane root, Button btn) {
+	//	HandlerClass handler = new HandlerClass();
+	//
+	//	btn.setOnAction(new EventHandler<ActionEvent>(){
+	//		@Override
+	//		public void handle(ActionEvent e){
+	//			//Code here.
+	//		}
+	//	});
+	//}
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+}
 
-	public void setPingBtnOnClick(Pane root, Button btn) {
-
+/**
+ * Event handler.
+ */
+class SetPingBtnOnClick implements EventHandler<ActionEvent>{
+	@Override
+	public void handle(ActionEvent e){
+		/*
+		 * Take in the domain names and save to each respective file name.
+		 */
+		runSystemCommand(domainOne.getText(), "domain1.txt");
+		runSystemCommand(domainTwo.getText(), "domain2.txt");
+		runSystemCommand(domainThree.getText(), "domain3.txt");
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	//FIXME Figure out what is wrong here.
+	public static void runSystemCommand(String command, String fileName){
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				try{
+					/*
+					 * Take 3 domains and start ping simultaneously.
+					 */
+					Process p = Runtime.getRuntime().exec(command);
+					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+					String s = "";
+
+					while((s = br.readLine()) != null){
+						writeToFiles(s, fileName);
+					}
+				}catch(IOException e){
+					System.out.println(e);
+				}
+			}
+		}).start();
+	}
+
+	public static void writeToFile(String s, String fileName){
+		try{
+			FileWriter fw = new FileWriter(fileName, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter out = new PrintWriter(bw);
+
+			out.println(s);
+			out.close();
+		}catch(IOException e){
+			System.out.println(e);
+		}
 	}
 }
--- PingIP.java
+++ PingIP.java
@@ -1,7 +1,8 @@
-/*
+/**
+ * From https://gist.github.com/madan712/4509039
  */
-import java.time.Instant;
-import java.time.Duration;
+//import java.time.Instant;
+//import java.time.Duration;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,7 +11,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-public class PingIP {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PingIP{
 	public static void runSystemCommand(String command, String fileName){
 		new Thread(new Runnable(){
 			@Override
@@ -24,7 +28,18 @@ public class PingIP {
 				
 					//Reading output stream of the command.
 					while((s = inputStream.readLine()) != null){
-						writeToFile(s, fileName);
+						/**
+						 * Create a new Date instance each time
+						 * it sends a packet and receives a 
+						 * response from the domain it is pinging.
+						 * Then, write both the ping data and date
+						 * it was received.
+						 */
+						Date sent = new Date();
+						writeToFile((sent + ""), fileName);
+						writeToFile(s + "\n", fileName);
+						Date received = new Date();
+						writeToFile((received + "\n"), fileName);
 					}
 
 				}catch(Exception e){
@@ -34,7 +49,7 @@ public class PingIP {
 		}).start();
 	}
  
-	public static void writeToFile(String s, String fileName) {
+	public static void writeToFile(String s, String fileName){
 		try{
 			FileWriter fw = new FileWriter(fileName, true);
 			BufferedWriter bw = new BufferedWriter(fw);
@@ -47,29 +62,47 @@ public class PingIP {
 		}
 	}
 
-	public static void sortString() {
+	public static void sortString(){
 		//?
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		/*
+		 * I added a bunch more domains but
+		 * I don't remember them all. The
+		 * formatting is more or less the
+		 * same though.
+		 */
 		String ip = "google.com";
 		String ip2 = "yahoo.com";
 		String ip3 = "facebook.com";
 
+		ArrayList<String> listOfDomains = new ArrayList<String>();
+		listOfDomains.add(ip);
+		listOfDomains.add(ip2);
+		listOfDomains.add(ip3);
+
+		for(String domain: listOfDomains){
+			runSystemCommand("ping -i 5 " + domain, domain + ".txt");
+		}
+
+		/**
+		 * XXX Possible DEAD code below.
+		 */
 		// Mac
 		//Start record of ping time.
-		Instant start = Instant.now();
-		System.out.println("Pinging Google...");
-		runSystemCommand("ping " + "-i 10 " + ip, "pings/google.txt");
+		//Instant start = Instant.now();
+		//System.out.println("Pinging Google...");
+		//runSystemCommand("ping " + "-i 10 " + ip, "pings/google.txt");
 		//End record of pinging.
-		Instant end = Instant.now();
-		Duration duration = Duration.between(start, end);
+		//Instant end = Instant.now();
+		//Duration duration = Duration.between(start, end);
 
-		System.out.println("Pinging Yahoo...");	
-		runSystemCommand("ping " + "-i 10 " + ip2, "pings/yahoo.txt");
+		//System.out.println("Pinging Yahoo...");	
+		//runSystemCommand("ping " + "-i 10 " + ip2, "pings/yahoo.txt");
 
-		System.out.println("Pinging Facebook...");
-		runSystemCommand("ping " + "-i 10 " + ip3, "pings/facebook.txt");
+		//System.out.println("Pinging Facebook...");
+		//runSystemCommand("ping " + "-i 10 " + ip3, "pings/facebook.txt");
 
 		// Windows
 		//runSystemCommand("ping " + "-w 10000 " + ip + " -t", "google.txt");
