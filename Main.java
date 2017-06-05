06fa45c9c62b971dde61c3f629ce9b3dcdc4789e
rcastr13 <rcastr13@calstatela.edu>
2017-06-05 15:38:35 -0700

    Add stable release of JavaFX version

--- Main.java
+++ Main.java
@@ -1,200 +1,187 @@
-/**
- * From https://gist.github.com/madan712/4509039
- */
+package application;
+	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-/**
- * Continued JavaFX implementations.
- */
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
-/**
- * Thread and File IO implementations.
- */
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
-public class Main extends Application{
+public class Main extends Application {
+	
+	TextField domainOne = new TextField();
+	TextField domainTwo = new TextField();
+	TextField domainThree = new TextField();
+	
+	PingIP pingTest = new PingIP();
+	
 	@Override
-	public void start(Stage primaryStage){	
-		/**
-		 * The box which contains everything.
-		 */
+	public void start(Stage primaryStage) {	
 		Pane root = new Pane();
 		
-		/**
-		 * Creates the layout of the form using
-		 * the root.
-		 */
 		createText(root);
 		createTextField(root);
 		createPingButton(root);
 		
-		/**
-		 * Area to place the form.
-		 */
-		Scene scene = new Scene(root, 300, 200);
+		Scene scene = new Scene(root, 275, 250);
 		
-		/**
-		 * Sets the scene to be shown on the pane.
-		 */
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	/**
-	 * Texts.
-	 */
-	public void createText(Pane root){
+	public void createText(Pane root) {
+		
 		Text one = new Text("1st Domain:");
 		one.setLayoutX(10);
 		one.setLayoutY(45);
-		one.setFont(Font.font ("Helvetica", 16));
-
+		one.setFont(Font.font ("helvetica", 16));
+		
 		Text two = new Text("2nd Domain:");
 		two.setLayoutX(10);
 		two.setLayoutY(85);
-		two.setFont(Font.font ("Helvetica", 16));
-
+		two.setFont(Font.font ("helvetica", 16));
+		
 		Text three = new Text("3rd Domain:");
 		three.setLayoutX(10);
 		three.setLayoutY(125);
-		three.setFont(Font.font ("Helvetica", 16));
+		three.setFont(Font.font ("helvetica", 16));
 		
-		/**
-		 * Places all text onto pane.
-		 */
 		root.getChildren().addAll(one, two, three);
+	
 	}
 	
-	/**
-	 * Input fields.
-	 */
-	public void createTextField(Pane root){
-		TextField domainOne = new TextField();
+	public void createTextField(Pane root) {
+		
 		domainOne.setMaxWidth(200);
 		domainOne.setLayoutX(115);
 		domainOne.setLayoutY(30);
-
-		TextField domainTwo = new TextField();
+		
+		
 		domainTwo.setMaxWidth(200);
 		domainTwo.setLayoutX(115);
 		domainTwo.setLayoutY(70);
-
-		TextField domainThree = new TextField();
+		
 		domainThree.setMaxWidth(200);
 		domainThree.setLayoutX(115);
 		domainThree.setLayoutY(110);
-
-		/**
-		 * Places all text fields onto pane.
-		 */
+	
 		root.getChildren().addAll(domainOne, domainTwo, domainThree);
+		
 	}
 	
-	/**
-	 * Button.
-	 */
-	public void createPingButton(Pane root){
+	public void createPingButton(Pane root) {
+		
 		Button btn = new Button("Start Test");	 
-		btn.setLayoutX(200);
+		btn.setStyle("-fx-font:	16 helvetica; -fx-base: green;");
+		btn.setLayoutX(175);
 		btn.setLayoutY(150);
 		
-		SetPingBtnOnClick btnClick = new SetPingBtnOnClick();
-
-//		btnClick(domainOne.getText());
-//		btnClick(domainTwo.getText());
-//		btnClick(domainThree.getText());
-
-		//setPingBtnOnClick(root, btn);
-
-		btn.setOnAction(btnClick);
-
-		/**
-		 * Places button onto pane.
-		 */
+		setPingBtnOnClick(root, btn);
+		
 		root.getChildren().add(btn);
 	}
 	
-	///**
-	// * Event handler.
-	// */
-	//public void setPingBtnOnClick(Pane root, Button btn) {
-	//	HandlerClass handler = new HandlerClass();
-	//
-	//	btn.setOnAction(new EventHandler<ActionEvent>(){
-	//		@Override
-	//		public void handle(ActionEvent e){
-	//			//Code here.
-	//		}
-	//	});
-	//}
-	
-	public static void main(String[] args){
-		launch(args);
-	}
-}
-
-/**
- * Event handler.
- */
-class SetPingBtnOnClick implements EventHandler<ActionEvent>{
-	@Override
-	public void handle(ActionEvent e){
-		/*
-		 * Take in the domain names and save to each respective file name.
-		 */
-		runSystemCommand(domainOne.getText(), "domain1.txt");
-		runSystemCommand(domainTwo.getText(), "domain2.txt");
-		runSystemCommand(domainThree.getText(), "domain3.txt");
-	}
-	
-	//FIXME Figure out what is wrong here.
-	public static void runSystemCommand(String command, String fileName){
-		new Thread(new Runnable(){
+	public void setPingBtnOnClick(Pane root, Button btn) {
+		
+		btn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
+			
 			@Override
-			public void run(){
-				try{
-					/*
-					 * Take 3 domains and start ping simultaneously.
-					 */
-					Process p = Runtime.getRuntime().exec(command);
-					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-					String s = "";
-
-					while((s = br.readLine()) != null){
-						writeToFiles(s, fileName);
-					}
-				}catch(IOException e){
-					System.out.println(e);
-				}
+			public void handle(Event event) {
+				
+				String one = domainOne.getText();
+				String two = domainTwo.getText();
+				String three = domainThree.getText();
+				
+				pingTest.setDomainOne(one);
+				pingTest.setDomainTwo(two);
+				pingTest.setDomainThree(three);
+				
+				stopScreen(root);
 			}
-		}).start();
+		});
 	}
-
-	public static void writeToFile(String s, String fileName){
-		try{
-			FileWriter fw = new FileWriter(fileName, true);
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter out = new PrintWriter(bw);
-
-			out.println(s);
-			out.close();
-		}catch(IOException e){
-			System.out.println(e);
-		}
+	
+	public void stopScreen(Pane root) {
+		
+		root.getChildren().clear();
+		
+		createStopButton(root);
+		
+		pingTest.runSystemCommand("ping " + pingTest.getDomainOne() + ".com -t", pingTest.getDomainOne() + ".txt");
+		pingTest.runSystemCommand("ping " + pingTest.getDomainTwo() + ".com -t", pingTest.getDomainTwo() + ".txt");
+		pingTest.runSystemCommand("ping " + pingTest.getDomainThree() + ".com -t", pingTest.getDomainThree() + ".txt");
+		
+	}
+	
+	public void createStopButton(Pane root) {
+		Button btn = new Button("Stop");
+		btn.setStyle("-fx-font:	24 helvetica; -fx-base: red;");
+		
+		btn.setLayoutX(105.5);
+		btn.setLayoutY(100);
+		
+		stopBtnOnClick(root, btn);
+		
+		root.getChildren().add(btn);
+	}
+	
+	public void stopBtnOnClick(Pane root, Button btn) {
+		btn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
+			
+			@Override
+			public void handle(Event event) {
+				pingTest.setStop(true);
+				showGraph();			
+			}
+		});
+	}
+	
+	public void showGraph() {
+		Stage stage = new Stage();
+		stage.setTitle("Ping Graph");
+		
+		final NumberAxis xAxis = new NumberAxis();
+		final NumberAxis yAxis = new NumberAxis();
+		
+		xAxis.setLabel("Time (Secs)");
+		
+		final LineChart<Number, Number> lineChart =
+				new LineChart<Number, Number>(xAxis, yAxis);
+		
+		lineChart.setTitle("Stuff");
+		XYChart.Series series1 = new XYChart.Series();
+		XYChart.Series series = new XYChart.Series();
+		series.setName("More stuff");
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series1.getData().add(new XYChart.Data(3, 15));
+        series1.getData().add(new XYChart.Data(4, 24));
+        series1.getData().add(new XYChart.Data(5, 34));
+        series1.getData().add(new XYChart.Data(6, 36));
+        series1.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+        
+        Scene scene = new Scene(lineChart, 800, 600);
+		lineChart.getData().addAll(series, series1);
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
--- PingIP.java
+++ PingIP.java
@@ -1,8 +1,4 @@
-/**
- * From https://gist.github.com/madan712/4509039
- */
-//import java.time.Instant;
-//import java.time.Duration;
+package application;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,13 +6,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-
-import java.util.ArrayList;
 import java.util.Date;
 
-public class PingIP{
-	public static void runSystemCommand(String command, String fileName){
-		new Thread(new Runnable(){
+public class PingIP {
+	
+	private String domainOne, domainTwo, domainThree;
+	private Boolean stop = false;
+
+	public PingIP() {}
+	
+	public PingIP(String domainOne, String domainTwo, String domainThree) {
+		
+		this.domainOne = domainOne;
+		this.domainTwo = domainTwo;
+		this.domainThree = domainThree;
+	}
+	
+  public void runSystemCommand(String command, String fileName) {
+	  
+	  new Thread(new Runnable(){
 			@Override
 			public void run(){
 				try{
@@ -27,7 +35,7 @@ public class PingIP{
 
 				
 					//Reading output stream of the command.
-					while((s = inputStream.readLine()) != null){
+					while((s = inputStream.readLine()) != null && !stop) {
 						/**
 						 * Create a new Date instance each time
 						 * it sends a packet and receives a 
@@ -35,11 +43,9 @@ public class PingIP{
 						 * Then, write both the ping data and date
 						 * it was received.
 						 */
-						Date sent = new Date();
-						writeToFile((sent + ""), fileName);
+			
 						writeToFile(s + "\n", fileName);
-						Date received = new Date();
-						writeToFile((received + "\n"), fileName);
+		
 					}
 
 				}catch(Exception e){
@@ -48,63 +54,55 @@ public class PingIP{
 			}
 		}).start();
 	}
- 
-	public static void writeToFile(String s, String fileName){
-		try{
-			FileWriter fw = new FileWriter(fileName, true);
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter out = new PrintWriter(bw);
+  
+  public void writeToFile(String s, String fileName) {
+	  
+	  try {
+		  	FileWriter fw = new FileWriter(fileName, true);
+		    BufferedWriter bw = new BufferedWriter(fw);
+		    PrintWriter out = new PrintWriter(bw);
+		    
+		    out.println(s);
+		    out.close();
+			
+	  } catch (IOException e) {
+			    
+	  }
+  }
+  
+  public void sortString() {
+	  
+  }
 
-			out.println(s);
-			out.close();
-		}catch(IOException e){
-			System.out.println(e);
-		}
+	public void setDomainOne(String domainOne) {
+		this.domainOne = domainOne;
 	}
 
-	public static void sortString(){
-		//?
+
+	public void setDomainTwo(String domainTwo) {
+		this.domainTwo = domainTwo;
 	}
 
-	public static void main(String[] args){
-		/*
-		 * I added a bunch more domains but
-		 * I don't remember them all. The
-		 * formatting is more or less the
-		 * same though.
-		 */
-		String ip = "google.com";
-		String ip2 = "yahoo.com";
-		String ip3 = "facebook.com";
 
-		ArrayList<String> listOfDomains = new ArrayList<String>();
-		listOfDomains.add(ip);
-		listOfDomains.add(ip2);
-		listOfDomains.add(ip3);
-
-		for(String domain: listOfDomains){
-			runSystemCommand("ping -i 5 " + domain, domain + ".txt");
-		}
-
-		/**
-		 * XXX Possible DEAD code below.
-		 */
-		// Mac
-		//Start record of ping time.
-		//Instant start = Instant.now();
-		//System.out.println("Pinging Google...");
-		//runSystemCommand("ping " + "-i 10 " + ip, "pings/google.txt");
-		//End record of pinging.
-		//Instant end = Instant.now();
-		//Duration duration = Duration.between(start, end);
-
-		//System.out.println("Pinging Yahoo...");	
-		//runSystemCommand("ping " + "-i 10 " + ip2, "pings/yahoo.txt");
-
-		//System.out.println("Pinging Facebook...");
-		//runSystemCommand("ping " + "-i 10 " + ip3, "pings/facebook.txt");
-
-		// Windows
-		//runSystemCommand("ping " + "-w 10000 " + ip + " -t", "google.txt");
+	public void setDomainThree(String domainThree) {
+		this.domainThree = domainThree;
 	}
+	
+	public String getDomainOne() {
+		return domainOne;
+	}
+
+	public String getDomainTwo() {
+		return domainTwo;
+	}
+
+
+	public String getDomainThree() {
+		return domainThree;
+	}
+	
+	public void setStop(Boolean stop) {
+		this.stop = stop;
+	}
+	
 }
